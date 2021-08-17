dab8f21acc2e032c85ede3936248dadb7d279fea
admin <paul002706@gmail.com>
2021-08-17 18:55:07 +0800

    test client <-> server

--- src/main/java/com/serverworld/worldSocketX/socket/SSLSocketClient.java
+++ src/main/java/com/serverworld/worldSocketX/socket/SSLSocketClient.java
@@ -60,6 +60,7 @@ public class SSLSocketClient {
                 DebugMessage.sendInfo("Loading login message...");
                 LoginMessage loginMessage = new LoginMessage(worldSocketXConfig.getUUID(),0);
                 out.println(loginMessage.getLoginJson());
+                out.println(loginMessage.getLoginJson());
                 DebugMessage.sendInfo("Login with: " + loginMessage.UUID);
                 DebugMessage.sendInfo("ProtocolVersion: " + 0);
                 while (true){

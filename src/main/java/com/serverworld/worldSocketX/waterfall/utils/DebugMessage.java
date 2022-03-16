03d719ecc57d7bf37d5749b10bdf2a2714bf1a2d
paul0665 <paul002706@gmail.com>
2022-03-16 09:07:14 +0800

    update copyright

--- src/main/java/com/serverworld/worldSocketX/waterfall/utils/DebugMessage.java
+++ src/main/java/com/serverworld/worldSocketX/waterfall/utils/DebugMessage.java
@@ -1,5 +1,5 @@
 /*
- *     Copyright (C) 2021  mc-serverworld
+ *     Copyright (C) 2022 mc-serverworld
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
